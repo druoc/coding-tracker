916fccefc650a8f6b5e00406ee5e2bff8c97dec6
Dave <d.ruocco1987@googlemail.com>
2024-01-30 11:12:50 +0000

    Initial commit, sets up project, SQLite and Spectre

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using Spectre.Console;
+
+
+namespace CodingTracker
+{
+    public class Program
+    {
+        static string connectionString = @"Data Source=coding-Tracker.db";
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS daily_coding (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        StartTime TEXT,
+                        EndTime TEXT,
+                        Duration TEXT
+                        )";
+
+                tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
